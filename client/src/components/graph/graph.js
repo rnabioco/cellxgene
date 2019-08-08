5afa051ef80a25929c2cd40b2cfcf44ffd462ca2
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2019-08-08 14:55:43 -0700

    fix issue with spaces in catagorical value name

--- client/src/components/graph/graph.js
+++ client/src/components/graph/graph.js
@@ -389,7 +389,7 @@ class Graph extends React.Component {
     if (pointDilation.categoryField) {
       svg
         ?.select(
-          `#svg${pointDilation.categoryField.replace(/[^\w\s]/gi, "")}-label`
+          `#svg${pointDilation.categoryField.replace(/[^\w]/gi, "")}-label`
         )
         .style("font-size", "18px")
         .style("font-weight", "800");
