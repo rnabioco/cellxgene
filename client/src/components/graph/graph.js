478540b03f9ba25eb7cad697a22bf1e15176ce2d
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2019-08-09 15:06:27 -0700

    remove logs

--- client/src/components/graph/graph.js
+++ client/src/components/graph/graph.js
@@ -335,9 +335,6 @@ class Graph extends React.Component {
       }
 
       const handleMouseEnter = d => {
-        console.log("colorAccessor", colorAccessor);
-        console.log("key", d.key);
-
         dispatch({
           type: "category value mouse hover start",
           metadataField: colorAccessor,
